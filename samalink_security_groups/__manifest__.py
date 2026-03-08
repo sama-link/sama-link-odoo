8afc3648abeab055e9cd7ab943c6d0fa6c4022c2
marwan wael <waelmarwan7@gmail.com>
2026-03-08 13:47:01 +0200

    fix manager 9

--- samalink_security_groups/__manifest__.py
+++ samalink_security_groups/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    'name': 'Samalink Security Groups v2.1.0',
-    'version': '2.1.0',
+    'name': 'Samalink Security Groups v2.2.0',
+    'version': '2.2.0',
     'summary': 'Manage security groups for Samalink',
     'description': 'Module to manage security groups and permissions in Samalink.',
     'author': '46-d-006',
